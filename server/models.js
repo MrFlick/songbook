da0a9a17c3e92bb9265fb21ed375d71040c070b6
Matthew Flickinger <mflick@umich.edu>
2019-05-22 00:57:10 -0400

    Add basic db models

--- server/models.js
+++ server/models.js
@@ -1,5 +1,58 @@
 const Sequelize = require('sequelize');
 
-module.exports = (sequelize) => {  
-  return {};
+const { Model } = Sequelize;
+
+module.exports = (sequelize) => {
+  class Album extends Model {}
+  Album.init({
+    showId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    showName: Sequelize.TEXT,
+    openingDate: Sequelize.DATE,
+  }, { sequelize, modelName: 'album' });
+  class Track extends Model {}
+  Track.init({
+    trackId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    trackName: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+  }, { sequelize, modelName: 'track' });
+  class Person extends Model {}
+  Person.init({
+    personId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    personName: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+  }, { sequelize, modelName: 'person' });
+  class Tag extends Model {}
+  Tag.init({
+    tagId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tagName: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    tagType: Sequelize.INTEGER,
+  }, { sequelize, modelName: 'tag' });
+  return {
+    Album,
+    Track,
+    Person,
+  };
 };
